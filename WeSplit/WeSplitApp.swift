e11172236914938a6c6ffe35c5c13b779bb21d28
ÄTAGWE ROGER <atagweroger2@gmail.com>
2026-06-25 13:55:52 +0100

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Atagwe Roger on 25/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
