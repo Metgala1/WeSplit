a88efb52072301ceb29fa69150977e9f19726476
ÄTAGWE ROGER <atagweroger2@gmail.com>
2026-07-07 09:55:15 +0100

    Dev commit

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -13,57 +13,67 @@ struct ContentView: View {
     @State private var tipPercentage = 20
     @FocusState private var amountIsFocused: Bool
     
-    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    let tipPercentages = [10 , 15 ,20 , 25, 0]
     var totalPerPerson: Double {
         let peopleCount = Double(numberOfPeople)
         let tipSelection = Double(tipPercentage)
         let tipValue = checkAmount / 100 * tipSelection
         let grandTotal = checkAmount + tipValue
+        
+        
         let amountPerPerson = grandTotal / peopleCount
         return amountPerPerson
     }
+    var totalCheckAmount: Double {
+        let tipSelection = Double(tipPercentage)
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        return grandTotal
+    }
     var body: some View {
         NavigationStack {
-        Form {
-            Section {
-                TextField("Amount ", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
-                    .keyboardType(.decimalPad)
-                    .focused($amountIsFocused)
-
-                Picker("Number of people", selection: $numberOfPeople) {
-                    ForEach(2..<100) { num in
-                    Text("\(num) people")
-                            .tag(num)
-                            
-                        
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    Picker("Number of people" , selection: $numberOfPeople) {
+                        ForEach(2..<100) { number in
+                            Text("\(number) people")
+                                .tag(number)
+                        }
                     }
+                    .pickerStyle(.navigationLink)
                 }
-                .pickerStyle(.navigationLink)
+                Section("How much do you want to tip?") {
+                    
+                    Picker("Tip Percentage" , selection: $tipPercentage) {
+                        ForEach(tipPercentages , id: \.self) { percentage in
+                            Text(percentage , format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                Section("Total Amount") {
+                    Text(totalCheckAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+                Section("Amount per person") {
+                    Text(totalPerPerson , format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
             }
-            Section("How much do you want to tip") {
-                
-                Picker("Tip percentage" , selection: $tipPercentage) {
-                    ForEach(tipPercentages , id: \.self) { tip in
-                        Text(tip, format: .percent)
+            .navigationTitle("WeSplit")
+            .toolbar {
+                if amountIsFocused {
+                    Button("Done") {
+                        amountIsFocused = false
                     }
                     
                 }
-                .pickerStyle(.segmented)
-            }
-            Section {
-                Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USd"))
-                    .foregroundStyle(tipPercentage == 0 ? .red : .black)
             }
         }
-        .navigationBarTitle("WeSplit")
-        .toolbar {
-            if amountIsFocused {
-                Button("Done") {
-                    amountIsFocused = false
-                }
-            }
-        }
-    }
+        
+        
   }
 }
 
